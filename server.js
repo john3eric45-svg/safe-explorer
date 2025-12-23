18f36568c0b92eae8c34ec5b194e9eb19bc8e3b2
john3eric45-svg <john3eric45@gmail.com>
2025-12-23 21:12:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const SESSION_TIMEOUT = 30 * 60 * 1000; // 30 min
 
 app.use(express.static('public'));
 app.use(express.json());
 
+const SESSION_TIMEOUT = 30 * 60 * 1000; // 30 min
 const activeSessions = new Map();
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.post('/api/start-session', (req, res) => {
-  const { level } = req.body;
+  const { level } = req.body || {};
   const sessionId = Date.now().toString();
   setTimeout(() => activeSessions.delete(sessionId), SESSION_TIMEOUT);
   activeSessions.set(sessionId, { level });
   res.json({ sessionId, timeLeft: SESSION_TIMEOUT });
-});
-
-app.get('*', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
 });
 
 module.exports = app;
